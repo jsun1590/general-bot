831100dac3f0b01b34f9b61ce5b326ce85e5c483
k9er <k9wolf@pm.me>
2022-07-20 09:15:53 -0700

    on -> once
    Minor revision

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.on('ready', () => {
+client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
